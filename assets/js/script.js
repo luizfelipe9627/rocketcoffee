8690126a507b5eb188f131634e1dd84358dcb884
Luiz Felipe <felipesilva9627@hotmail.com>
2022-07-08 00:37:33 -0300

    Código mais limpo.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,12 @@
-// Responsável por abrir e fechar o menu.
+// Responsável por abrir e fechar o menu e trocar o ícone.
 window.onload = function () {
   document.querySelector(".menu-mobile").addEventListener("click", function () {
     if (document.querySelector(".menu nav").style.display == "flex") {
       document.querySelector(".menu nav").style.display = "none";
+      document.querySelector(".menu-mobile").src = "https://luizfelipe9627.github.io/rocketcoffee/assets/image/menu-hamburguer-close.svg"
     } else {
       document.querySelector(".menu nav").style.display = "flex";
+      document.querySelector(".menu-mobile").src = "https://luizfelipe9627.github.io/rocketcoffee/assets/image/menu-hamburguer-open.svg"
     }
   });
 };
-
-// Responsável por mudar o ícone do menu clicável.
-function clicou() {
-  if (document.querySelector(".menu nav").style.display == "flex") {
-    document.querySelector(".menu-mobile").src =
-      "https://luizfelipe9627.github.io/rocketcoffee/assets/image/menu-buguer-open.svg";
-  } else if ((document.querySelector(".menu nav").style.display = "none")) {
-    document.querySelector(".menu-mobile").src =
-      "https://luizfelipe9627.github.io/rocketcoffee/assets/image/menu-buguer-close.svg";
-  }
-}
